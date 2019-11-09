e5bc40073a178a98ddb8db947e4fed4dbbda9cfd
Leona <zur0037@vsb.cz>
2019-11-09 20:37:08 +0100

    Not walking through mobile frame

--- app/src/main/java/com/example/sokoban33/GameObject.java
+++ app/src/main/java/com/example/sokoban33/GameObject.java
@@ -1,38 +1,47 @@
 package com.example.sokoban33;
 
+import android.util.Log;
+
 public class GameObject {
-    public int posX;
-    public int posY;
-    //public final int[] LEVEL;
+    public int posX, posY;
+    public boolean canGoUp, canGoLeft, canGoRight, canGoDown;
 
     GameObject(int x, int y){
         this.posX = x;
         this.posY = y;
-        //this.LEVEL = E.LEVELS[0];
     }
+
     public static int getCoor(int x, int y){
         return y*10+x;
     }
-    public int heroCoor() {return this.posY*10+this.posX;}
-    public void move(int direction) {
 
-        E.actualLevel[heroCoor()] = 0;
+    public int heroCoor() {return this.posY*10+this.posX;}
+
+    public void move(int direction) {
+        canGoRight = (this.posX<9);
+        canGoLeft = (this.posX>0);
+        canGoUp = (this.posY>0);
+        canGoDown = (this.posY<9);
+        //Log.i("HeroPos", heroCoor()+": UP?"+canGoUp+" DOWN?"+canGoDown+" LEFT?"+canGoLeft+" RIGHT?"+canGoRight);
+
+        E.actualLevel[heroCoor()] = E.EMPTY; //sma
 
         switch(direction){
             case E.RIGHT:
-                this.posX++;
+                if(this.canGoRight)this.posX++;
                 break;
             case E.LEFT:
-                this.posX--;
+                if(this.canGoLeft)this.posX--;
                 break;
             case E.UP:
-                this.posY--;
+                if(this.canGoUp)this.posY--;
                 break;
             case E.DOWN:
-                this.posY++;
+                if(this.canGoDown)this.posY++;
                 break;
         }
-        E.actualLevel[heroCoor()] = 4;
+
+        E.actualLevel[heroCoor()] = E.HERO;
 
     }
 }
